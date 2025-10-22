26b7282d8da7b3ed14212cc698c9ae4a85c89ca4
Kinjiam <maya.jimena@yahoo.com>
2025-10-21 18:04:37 -0600

    Modify TR_LogsClientes trigger to include 'Tabla'
    Add 'Tabla' column to LogsUsuarios insert statement.

--- scripts/triggers/TR_LogsClientes.Trigger.sql
+++ scripts/triggers/TR_LogsClientes.Trigger.sql
@@ -5,7 +5,7 @@ ON Clientes  -- Donde se va a crear
 AFTER INSERT, UPDATE  --Después de que acción se va a ejecutar
 AS
 BEGIN
-    INSERT INTO dbo.LogsUsuarios(Usuario, Rol, Accion)
+    INSERT INTO dbo.LogsUsuarios(Usuario, Rol, Tabla, Accion)
     -- En que tabla se va a crear una nueva fila
                                                     -- Indicando que columnas se van a llenar
     SELECT SYSTEM_USER, --El usuario que genera el movimiento
